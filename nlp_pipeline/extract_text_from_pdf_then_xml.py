091720cfcf77065977f3b7e91eb4a147167bc962
Yijie-Ren-1 <76422749+Yijie-Ren-1@users.noreply.github.com>
2021-04-19 22:28:09 -0500

    Update extract_text_from_pdf_then_xml.py

--- nlp_pipeline/extract_text_from_pdf_then_xml.py
+++ nlp_pipeline/extract_text_from_pdf_then_xml.py
@@ -1,6 +1,7 @@
 '''
 This file is to convert pdf file to txt file, then format the converted txt file to BioC xml file. BioC xml file is
 one of the formats which can be submitted to Pubtator tool. Other possible formats can be Json or Pubtator format.
+
 This file can process a bunch of pdf files at the same time, just make sure all the pdf files are under the same folder.
 '''
 
@@ -10,9 +11,10 @@ import os
 import sys
 import xml.etree.ElementTree as ET
 import re
+import shutil
 
 
-def convert_pdf_as_text_file(source, destination_folder):
+def convert_pdf_to_text_file(source, destination_folder):
     '''
     :param source: str, source pdf file path
     :param destination_folder: str, txt file destination folder path
@@ -26,7 +28,7 @@ def convert_pdf_as_text_file(source, destination_folder):
         f.write(paperInText)
 
 
-def convert_txt_as_BioC_xml_file(source, destination_folder):
+def convert_txt_to_BioC_xml_file(source, destination_folder):
     '''
     :param source: str, source txt file path
     :param destination_folder: str, BioC xml file destination folder path
@@ -39,25 +41,39 @@ def convert_txt_as_BioC_xml_file(source, destination_folder):
     sep_sents = []
     for string in sents:
         string = string.replace('\n', ' ')
-        sep_sents.append(re.sub('\W+', ' ', string))
+        sep_sents.append(re.sub('\W+',' ', string))
 
-
-    collection = ET.Element('collection')
-    document = ET.SubElement(collection, 'document')
+    grouped_sents = []
+    sep_sents_len = len(sep_sents)
+    n = 10
+    for i in range(n):
+        start = int(i * sep_sents_len / n)
+        end = int((i + 1) * sep_sents_len / n)
+        grouped_sents.append('. '.join(sep_sents[start:end]))
 
     offset_count = 0
-    for sent in sep_sents:
+    txtName = source[:-4]
+    xml_folder_name = os.path.basename(txtName)
+
+    shutil.rmtree(destination_folder + xml_folder_name, ignore_errors=True)
+    if not os.path.isdir(destination_folder + xml_folder_name):
+        os.mkdir(destination_folder + xml_folder_name)
+
+    xml_file_counter = 0
+    for sent in grouped_sents:
+        xml_file_counter += 1
+        collection = ET.Element('collection')
+        document = ET.SubElement(collection, 'document')
         passage = ET.SubElement(document, 'passage')
         offset = ET.SubElement(passage, 'offset')
         offset.text = str(offset_count)
-        # offset_count += 1
         text = ET.SubElement(passage, 'text')
         text.text = sent
 
-    tree = ET.ElementTree(collection)
-    txtName = source[:-4]
-    filename = destination_folder + os.path.basename(txtName) + '.xml'
-    tree.write(filename, encoding="UTF-8")
+        tree = ET.ElementTree(collection)
+
+        filename = destination_folder + xml_folder_name + '/' + str(xml_file_counter) + '.xml'
+        tree.write(filename, encoding="UTF-8")
 
 if __name__ == "__main__":
     source_pdf_folder = './pdf/'
@@ -68,10 +84,13 @@ if __name__ == "__main__":
     for pdf_file in glob.glob(source_pdf_folder + "*.pdf"):
         if not os.path.isdir(txt_destination_folder):
             os.mkdir(txt_destination_folder)
-        convert_pdf_as_text_file(pdf_file, txt_destination_folder)
+        convert_pdf_to_text_file(pdf_file, txt_destination_folder)
 
     # convert txt file to BioC xml file
     for txt_file in glob.glob(txt_destination_folder + "*.txt"):
         if not os.path.isdir(BioC_xml_destination_folder):
             os.mkdir(BioC_xml_destination_folder)
-        convert_txt_as_BioC_xml_file(txt_file, BioC_xml_destination_folder)
+        convert_txt_to_BioC_xml_file(txt_file, BioC_xml_destination_folder)
+
+
+
